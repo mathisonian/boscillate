cbd856328259e45cd0f61dc42bc01e6710898a7d
Matthew Conlen <mc@mathisonian.com>
2014-06-25 23:57:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,57 @@ boscillate
 ==========
 
 Sound wave graph for baudio in your terminal
+
+
+## usage
+
+`npm install boscillate`
+
+```js 
+var baudio = require('baudio');
+var oscillate = require('boscillate');
+
+var n = 0;
+var b = baudio(function (t, i) {
+    return Math.sin(t * 400 * Math.PI * 2) + Math.sin(t * 500) * (t % 2 > 1);
+});
+
+
+b = oscillate(b);
+b.play();
+```
+
+This pretty much take over your terminal
+
+## options
+
+
+```js
+b = oscillate(b, {
+    framesPerSecond: 20, // how often does this try to refresh the screen
+                         // higher -> faster & more intensive
+                         // find a value that works for your terminal size
+    
+    windowSize: 1000     // window size: 
+                         // the graphs are composed of a sliding window of 
+                         // time series values.
+                         // e.g. if we got values 1, 2, 3, 4, 5, 6, 7
+                         //      then with a window of size two, the graph
+                         //      would display the following two points at
+                         //      each refresh:
+                         //      [1, 2], 3, 4, 5, 6, 7
+                         //      1, [2, 3], 4, 5, 6, 7
+                         //      1, 2, [3, 4], 5, 6, 7
+                         //      1, [2, 3, [4, 5], 6, 7
+                         //      1, 2, 3, 4, [5, 6], 7
+                         //      1, 2, 3, 4, 5, [6, 7]
+                         //      etc
+                         //
+                         // so a bigger window size means you can see further into
+                         // the past on the audio wave
+
+})
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,95 @@
+// 
+'use strict';
+var Canvas = require('drawille');
+var _ = require('lodash');
+require('colors');
+
+var width = process.stdout.getWindowSize()[0] * 2;
+var height = process.stdout.getWindowSize()[1] * 4;
+var canvas = new Canvas(width, height);
+
+
+Number.prototype.mod = function(n) {
+    return ((this%n)+n)%n;
+};
+
+// re-used from baudio
+function mergeArgs (opts, args) {
+    Object.keys(opts || {}).forEach(function (key) {
+        args[key] = opts[key];
+    });
+    
+    return Object.keys(args).reduce(function (acc, key) {
+        var dash = key.length === 1 ? '-' : '--';
+        return acc.concat(dash + key, args[key]);
+    }, []);
+}
+
+
+var oscillate = module.exports =  function(b, options) {
+    
+    options  = _.defaults(options || {}, {
+        framesPerSecond: 20,
+        windowSize: 1000
+    });
+
+    // 
+    // pipe from baudio -> boscillate -> sox
+    //
+    //
+    var Transform = require('stream').Transform;
+    var ts = new Transform();
+    var v = 0;
+    var windowSize = options.windowSize;
+    var points = Array.apply(null, new Array(windowSize)).map(Number.prototype.valueOf,0);
+    ts._transform = function (chunk, encoding, done) {
+        for (var i = 0; i < chunk.length; i += 2) {
+            v++;
+            var n = chunk.readInt16LE(i);
+            points[v % windowSize] = n;
+        }
+        this.push(chunk);
+        done();
+    };
+    
+    function draw() {
+        canvas.clear();
+        var offset = v % windowSize;
+        for(var i=0; i<points.length; i++) {
+            var point = points[i];
+            var index = (i - offset).mod(windowSize);
+            canvas.set(width - width * (index / windowSize), height / 2 + point / 1000);
+        }
+        console.log(canvas.frame().green);
+    }
+
+    b.play = function (opts) {
+        var ps = this._spawn('play', mergeArgs(opts, {
+            'c': 1,
+            'r': this.rate,
+            't': 's16'
+        }).concat('-', '-q'));
+        this.pipe(ts).pipe(ps.stdin);
+        // draw in the interval to avoid mucking
+        // up the pipes
+        setInterval(draw, 1 / options.framesPerSecond * 1000);
+        return ps;
+    };
+
+    return b;
+};
+var baudio = require('baudio');
+// var oscillate = require('boscillate');
+
+var n = 0;
+var b = baudio(function (t, i) {
+    return Math.sin(t * 400 * Math.PI * 2) + Math.sin(t * 500) * (t % 2 > 1);
+});
+
+
+b = oscillate(b);
+b.play();
+
+
+
+
